19597eca7417d3a986985104d7b823bf3509756b
Justin Szaday <jjszaday@gmail.com>
2022-04-17 09:09:45 -0400

    Clean up constructor options

--- include/ck/options.hpp
+++ include/ck/options.hpp
@@ -6,21 +6,6 @@
 
 namespace ck {
 
-namespace {
-template <std::size_t N>
-std::array<int, N> __to_integer_array(const void* data) {
-  std::array<int, N> res;
-  if constexpr (N <= CK_ARRAYINDEX_MAXLEN) {
-    auto* ints = reinterpret_cast<const int*>(data);
-    std::copy(ints, ints + N, res.begin());
-  } else {
-    auto* shorts = reinterpret_cast<const short*>(data);
-    std::transform(shorts, shorts + N, res, [](short s) { return (int)s; });
-  }
-  return res;
-}
-}  // namespace
-
 template <typename T, typename Enable = void>
 struct constructor_options;
 
@@ -28,25 +13,49 @@ template <typename T>
 struct constructor_options<T, std::enable_if_t<is_array_v<kind_of_t<T>>>> {
   using index_t = index_of_t<kind_of_t<T>>;
   using array_index_t = array_index_of_t<index_t>;
-  static constexpr auto N = dimensionality_of_v<index_t>;
 
+ private:
+  template <typename... Ts>
+  static auto __get_dimension(Ts&&... ts) {
+    short result;
+    auto fn = [](const auto& idx) { return idx.getDimension(); };
+    auto pred = [&](const auto& idx) { return (result == fn(idx)); };
+    // get the dimension of the first index
+    ((result = fn(ts), true) || ...);
+    // check that all indices have the same dimension
+    if constexpr (sizeof...(ts) >= 1) {
+      auto count = (std::size_t(0) + ... + (pred(ts) ? 1 : 0));
+      CkEnforceMsg((count == sizeof...(ts)), "dimensionality mismatch");
+    }
+    return result;
+  }
+
+ public:
   CkArrayOptions array;
 
   constructor_options(const index_t& stop)
-      : array(N, __to_integer_array<N>(&stop).data()) {}
+      : constructor_options(index_view<index_t>::encode(stop)) {}
 
   constructor_options(const array_index_t& stop)
-      : constructor_options(*reinterpret_cast<const index_t*>(stop.data())) {}
+      : array(CkArrayIndex(__get_dimension(stop), 0), stop,
+              CkArrayIndex(__get_dimension(stop), 1)) {}
 
-  constructor_options(const array_index_t& start, const array_index_t& stop,
-                      const array_index_t& step)
-      : array(start, stop, step) {}
+  constructor_options(const index_t& start, const index_t& stop)
+      : constructor_options(index_view<index_t>::encode(start),
+                            index_view<index_t>::encode(stop)) {}
+
+  constructor_options(const array_index_t& start, const array_index_t& stop)
+      : array(start, stop, CkArrayIndex(__get_dimension(start, stop), 1)) {}
 
   constructor_options(const index_t& start, const index_t& stop,
                       const index_t& step)
       : constructor_options(index_view<index_t>::encode(start),
                             index_view<index_t>::encode(stop),
                             index_view<index_t>::encode(step)) {}
+
+  constructor_options(const array_index_t& start, const array_index_t& stop,
+                      const array_index_t& step)
+      : array(start, stop, step) {}
 };
 }  // namespace ck
 
