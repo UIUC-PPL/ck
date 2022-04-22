4722c3ede94372a62cd31465478c466871c52f2c
Justin Szaday <jjszaday@gmail.com>
2022-04-22 19:18:17 -0400

    add comments to `readonly.hpp`

--- include/ck/readonly.hpp
+++ include/ck/readonly.hpp
@@ -3,48 +3,43 @@
 
 #include <ck/traits.hpp>
 
+// generate a readonly impl-name for a variable
 #define CK_READONLY_NAME(name) __##name##__readonly
 
-#define CK_READONLY_IMPL(name) ck::readonly<CK_READONLY_NAME(name)>
-
+// generate a readonly impl-class for a variable
+// along with its global (reference) declaration
 #define CK_READONLY(type, name)               \
   struct CK_READONLY_NAME(name) {             \
     type value;                               \
     void pup(PUP::er& p) { p | this->value; } \
   };                                          \
-  type& name = (CK_READONLY_IMPL(name)::__access()).value;
+  type& name = (ck::readonly<CK_READONLY_NAME(name)>::__access()).value;
 
+// forward declares a readonly variable
 #define CK_EXTERN_READONLY(type, name) extern type& name;
 
 namespace ck {
 
+// helper class to pup, register, and store a readonly variable
 template <typename T>
 struct readonly {
   static int __idx;
-  static storage_t<T> __storage;
 
+  // accesses this readonly variable's stored value
   static T& __access(void) {
     // force the compiler to initialize this variable
     __dummy(readonly<T>::__idx);
-    // initialize readonly during first access
-    static bool initialized = false;
-    auto& t = reinterpret_cast<T&>(__storage);
-    if (!initialized) {
-      if constexpr (std::is_constructible_v<T, PUP::reconstruct>) {
-        new (&t) T(PUP::reconstruct());
-      } else {
-        new (&t) T;
-      }
-      initialized = true;
-    }
-    return t;
+    static PUP::detail::TemporaryObjectHolder<T> val;
+    return val.t;
   }
 
+  // pups this readonly variable
   static void __pup(void* p_) {
     auto& p = *((PUP::er*)p_);
     p | __access();
   }
 
+  // registers this readonly variable with the RTS
   static void __register(void) {
     auto* name = typeid(T).name();
 #if CMK_VERBOSE
@@ -54,6 +49,7 @@ struct readonly {
   }
 };
 
+// adds a readonly to the list of readonlies registered at startup
 template <typename T>
 int readonly<T>::__idx = ([](void) {
   auto& reg = registry::readonlies();
@@ -61,9 +57,6 @@ int readonly<T>::__idx = ([](void) {
   reg.emplace_back(&(readonly<T>::__register));
   return idx;
 })();
-
-template <typename T>
-storage_t<T> readonly<T>::__storage;
 }  // namespace ck
 
 #endif
