181b2bd1a20462ddf5cad72dea12f58883162050
Justin Szaday <jjszaday@gmail.com>
2022-04-23 18:23:05 -0400

    Automate detection of run-flags

--- tests/run.sh
+++ tests/run.sh
@@ -1,25 +1,39 @@
 #!/bin/bash
 set -euo pipefail
 IFS=$'\n\t'
-
-make clean
-make -j
-
+# ensure charm path correctly specified
+basedir=${CHARM_HOME:-}
+if [ -z "$basedir" ] || [ ! -d "$basedir" ]; then
+    echo "error> set CHARM_HOME=/path/to/charm"
+    exit 1
+else
+    set +u
+    # source machine options to learn build configuration
+    source "$basedir/include/conv-mach-opt.sh"
+    set -u
+fi
+# detect the number of processors to run with
 if [ "$OSTYPE" = "linux-gnu" ]; then
     nproc=$(grep ^cpu\\scores /proc/cpuinfo | uniq |  awk '{print $4}')
 else
     nproc=$(nproc)
 fi
-
+# detect netlrts builds and set all-test options accordingly
+if [ "$CMK_GDIR" = "netlrts" ]; then
+    allargs="++local"
+else
+    allargs=""
+fi
+# set per-test options
 unset args
 declare -A args
-
 args["histogram"]="4 4 4 4"
 args["leanmd"]="3 3 3 20"
 args["hierarchical_hello"]="4 4 +balancer RotateLB +LBDebug 1"
-
-allargs="++local"
-
+# clean and build all tests 
+make clean
+make -j
+# then get ready to rock n' roll
 for pgm in *.out; do
     key="${pgm%%.*}"
     set +u
